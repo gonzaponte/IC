d6a688d7d77037545ea02417d260a92c96f0c671
Gonzalo <gonzaponte@gmail.com>
2017-02-23 16:23:43 +0100

    database will return a class, not separate values

--- invisible_cities/core/light_tables.py
+++ invisible_cities/core/light_tables.py
@@ -21,16 +21,16 @@ class LightTables:
         self._load()
 
     def PMT(self, x, y):
-        xbin = int((x-self.xmin)//self.pmt_pitch)
-        ybin = int((y-self.ymin)//self.pmt_pitch)
+        xbin = int((x-self.geo.xmin)//self.pmt_pitch)
+        ybin = int((y-self.geo.ymin)//self.pmt_pitch)
         return self._pmt[xbin, ybin]
 
     def SiPM(self, x, y):
-        xbin = int((x-self.xmin)//self.pmt_pitch)
-        ybin = int((y-self.ymin)//self.pmt_pitch)
+        xbin = int((x-self.geo.xmin)//self.pmt_pitch)
+        ybin = int((y-self.geo.ymin)//self.pmt_pitch)
         return self._sipm[xbin][ybin]
 
     def _load(self):
-        self.xmin, self.xmax, self.ymin, self.ymax = DB.DataDetector()
+        self.geo = DB.DataDetector()
         self. pmt_pitch, self._pmt  = DB.TablePMT()
         self.sipm_pitch, self._sipm = DB.TableSiPM()
