f71a496e4f66c165fd2f792278aec2862b3ce1fc
Jose A. Hernando <hernando.joseangel@gmail.com>
2018-01-09 23:43:57 +0100

    Add city decorator

--- invisible_cities/city_flows/city_components.py
+++ invisible_cities/city_flows/city_components.py
@@ -1,9 +1,50 @@
+from collections import Sequence
+from functools   import wraps
+from glob        import glob
+from os.path     import expandvars
+
 from .. evm.ic_containers import SensorData
 from .. evm.ic_containers import EventData
 from .. reco              import tbl_functions as tbl
 from .. database          import load_db
 from .. sierpe            import blr
 
+from .. core.exceptions         import NoInputFiles
+from .. core.exceptions         import NoOutputFile
+
+
+def city(city_function):
+    @wraps(city_function)
+    def proxy(**kwds):
+        conf = Namespace(**kwds)
+
+        # TODO Check raw_data_type in parameters for RawCity
+
+        if 'files_in'  not in kwds: raise NoInputFiles
+        if 'files_out' not in kwds: raise NoOutputFile
+
+        conf.input_files  = sorted(glob(expandvars(conf.files_in)))
+        conf.output_file  =             expandvars(conf.file_out)
+
+        conf.event_range  = _event_range(conf)
+        # TODO There were deamons! self.daemons = tuple(map(summon_daemon, kwds.get('daemons', [])))
+
+        return city_function(**vars(conf))
+    return proxy
+
+def _event_range(conf):
+    if not hasattr(conf, 'event_range'):        return None, 1
+    er = conf.event_range
+    if not isinstance(er, Sequence): er = (er,)
+    if len(er) == 1:                            return None, er[0]
+    if len(er) == 2:                            return tuple(er)
+    if len(er) == 0:
+        raise ValueError('event_range needs at least one value')
+    if len(er) >  2:
+        raise ValueError('event_range accepts at most 2 values but was given {}'
+                        .format(er))
+
+############################
 
 def event_data_from_files(paths):
     for path in paths:
@@ -13,7 +54,6 @@ def event_data_from_files(paths):
             events_infos            = h5in.root.Run.events
             for pmt, sipm, mc, event_info in zip(pmtrwfs, sipmrwfs, mc_tracks, events_info):
                 yield EventData(pmt=pmt, sipm=sipm, mc=mc, event_info=event_info)
-
 
 
 def deconv_pmt(n_baseline):
--- invisible_cities/city_flows/isidora.py
+++ invisible_cities/city_flows/isidora.py
@@ -13,21 +13,22 @@ from .. io.           mc_io import      mc_track_writer
 from .. io.          rwf_io import           rwf_writer
 from .. io.run_and_event_io import run_and_event_writer
 
+from city_components import city
 from city_components import deconv_pmt
 from city_components import event_data_from_files
 from city_components import sensor_data
 
-################################################################################
 
-def isidora(input_paths, output_path, run_number=0, n_baseline=30000, compression='ZLIB4'):
+@city
+def isidora(files_in, file_out, event_range, run_number, n_baseline, compression):
     """
     The city of ISIDORA performs a fast processing from raw data
     (pmtrwf and sipmrwf) to BLR wavefunctions.
 
     """
-    sd = sensor_data(input_paths[0])
+    sd = sensor_data(files_in[0])
 
-    h5out = tb.open_file(output_path, "w", filters = tbl.filters(compression))
+    h5out = tb.open_file(file_out, "w", filters = tbl.filters(compression))
     rwf_to_cwf = df.map(deconv_pmt(n_baseline))
 
     RWF = partial(rwf_writer, h5out, group_name='BLR')
@@ -38,18 +39,11 @@ def isidora(input_paths, output_path, run_number=0, n_baseline=30000, compressio
 
     event_count = df.count()
 
-    return df.push(source  = event_data_from_files(input_paths),
-                   pipe    = df.fork((df.map(attrgetter('pmt'       )), rwf_to_cwf, writer_pmt       ),
-                                     (df.map(attrgetter('sipm'      )),             writer_sipm      ),
-                                     (df.map(attrgetter('mc'        )),             writer_mc        ),
-                                     (df.map(attrgetter('event_info')),             writer_event_info),
-                                     event_count.sink),
-                   futures = (event_count.future,))
-
-    return df.push(source  = event_data_from_files(input_paths),
-                   pipe    = df.fork((df.pick('pmt'       ), rwf_to_cwf, writer_pmt       ),
-                                     (df.pick('sipm'      ),             writer_sipm      ),
-                                     (df.pick('mc'        ),             writer_mc        ),
-                                     (df.pick('event_info'),             writer_event_info),
-                                     event_count.sink),
+    return df.push(source  = event_data_from_files(files_in),
+                   pipe    = df.pipe(df.slice(*event_range),
+                    df.fork((df.pick('pmt'       ), rwf_to_cwf, writer_pmt       ),
+                            (df.pick('sipm'      ),             writer_sipm      ),
+                            (df.pick('mc'        ),             writer_mc        ),
+                            (df.pick('event_info'),             writer_event_info),
+                            event_count.sink),
                    futures = (event_count.future,))
