11c13c5a33c8e08f8f98fd5b86eddabcf32ab866
Jacek Generowicz <github@my-post-office.net>
2018-01-10 23:59:01 +0100

    Remove 'df.' qualification where confusion is unlikely

--- invisible_cities/city_flows/isidora.py
+++ invisible_cities/city_flows/isidora.py
@@ -4,12 +4,19 @@ from functools import partial
 import numpy  as np
 import tables as tb
 
-from .. dataflow            import dataflow      as df
 from .. reco                import tbl_functions as tbl
 from .. database            import load_db
 from .. sierpe              import blr
 from .. io.          rwf_io import           rwf_writer
 from .. io.run_and_event_io import run_and_event_writer
+
+from .. dataflow            import dataflow      as df
+from .. dataflow.dataflow   import     sink
+from .. dataflow.dataflow   import starsink
+from .. dataflow.dataflow   import push
+from .. dataflow.dataflow   import pipe
+from .. dataflow.dataflow   import pick
+from .. dataflow.dataflow   import fork
 
 from .  city_components import city
 from .  city_components import deconv_pmt
@@ -33,19 +40,20 @@ def isidora(files_in, file_out, event_range, run_number, n_baseline,
     with tb.open_file(file_out, "w", filters = tbl.filters(compression)) as h5out:
 
         RWF = partial(rwf_writer, h5out, group_name='BLR')
-        writer_pmt   = df.sink(RWF(table_name='pmtcwf' , n_sensors=sd.NPMT , waveform_length=sd.PMTWL))
-        writer_sipm  = df.sink(RWF(table_name='sipmrwf', n_sensors=sd.NSIPM, waveform_length=sd.SIPMWL))
-        writer_event_info = df.starsink(run_and_event_writer(h5out))
+        writer_pmt   = sink(RWF(table_name='pmtcwf' , n_sensors=sd.NPMT , waveform_length=sd.PMTWL))
+        writer_sipm  = sink(RWF(table_name='sipmrwf', n_sensors=sd.NSIPM, waveform_length=sd.SIPMWL))
+        writer_event_info = starsink(run_and_event_writer(h5out))
 
         writer_mc = make_writer_mc(h5out)
 
         event_count = df.count()
 
-        return df.push(source = event_data_from_files(files_in),
-                       pipe   = df.pipe(#df.slice(*event_range),
-                        df.fork(df.pipe(df.pick('pmt'       ), rwf_to_cwf, writer_pmt       ),
-                                df.pipe(df.pick('sipm'      ),             writer_sipm      ),
-                                df.pipe(df.pick('mc'        ),             writer_mc        ),
-                                df.pipe(df.pick('event_info'),             writer_event_info),
-                                event_count.sink)),
-                       result = (event_count.future,))
+        return push(
+            source = event_data_from_files(files_in),
+            pipe   = pipe(#df.slice(*event_range),
+             fork(pipe(pick('pmt'       ), rwf_to_cwf, writer_pmt       ),
+                  pipe(pick('sipm'      ),             writer_sipm      ),
+                  pipe(pick('mc'        ),             writer_mc        ),
+                  pipe(pick('event_info'),             writer_event_info),
+                  event_count.sink)),
+            result = (event_count.future,))
