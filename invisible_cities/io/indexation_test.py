cb714498c3190f94a1a6d531137c2a6cd49dba0f
Jacek Generowicz <github@my-post-office.net>
2018-10-10 19:28:55 +0200

    liquid_cities -> cities

--- invisible_cities/io/indexation_test.py
+++ invisible_cities/io/indexation_test.py
@@ -4,7 +4,7 @@ import tables as tb
 from pytest import mark
 from pytest import fixture
 
-from .. liquid_cities.components import city
+from .. cities.components import city
 from .. core  .configure   import configure
 
 from . hits_io   import     hits_writer
