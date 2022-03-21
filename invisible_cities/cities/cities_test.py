f005edafd185d4ad018fc5021dddcdecca841eed
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2022-03-21 14:44:40 +0200

    Add test to ensure config is written in cities

--- invisible_cities/cities/cities_test.py
+++ invisible_cities/cities/cities_test.py
@@ -1,13 +1,19 @@
 import os
 
+import tables as tb
+
 from importlib import import_module
 from pytest import mark
 from .. core.configure      import configure
 
-cities = "irene dorothea penthesilea esmeralda beersheba".split()
+all_cities = ("beersheba berenice buffy detsim diomira"
+              "dorothea esmeralda eutropia hypathia irene"
+              "isaura isidora penthesilea phyllis trude").split()
+
+lsc_cities = "irene dorothea penthesilea esmeralda beersheba".split()
 
 @mark.filterwarnings("ignore::UserWarning")
-@mark.parametrize("city", cities)
+@mark.parametrize("city", lsc_cities)
 def test_city_empty_input_file(config_tmpdir, ICDATADIR, city):
     # All cities run in Canfranc must run on an empty file
     # without raising any exception
@@ -24,3 +30,22 @@ def test_city_empty_input_file(config_tmpdir, ICDATADIR, city):
     city_function = getattr(import_module(module_name), city)
 
     city_function(**conf)
+
+
+@mark.filterwarnings("ignore::UserWarning")
+@mark.parametrize("city", all_cities)
+def test_city_contains_config(config_tmpdir, ICDATADIR, city):
+    output_file = os.path.join(config_tmpdir, f"test_{city}_contains_config.h5")
+    config_file = 'dummy invisible_cities/config/{}.conf'.format(city)
+
+    conf = configure(config_file.split())
+    conf.update(dict(file_out = output_file))
+
+    module_name   = f'invisible_cities.cities.{city}'
+    city_function = getattr(import_module(module_name), city)
+
+    city_function(**conf)
+
+    with tb.open_file(output_file) as file:
+        assert "Config" in file.root
+        assert    city  in file.root.Config
