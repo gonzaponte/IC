649763c1e38ffab4d3b290178c8150ee55c720a4
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2023-01-05 15:33:28 +0200

    Unfreeze package versions

--- manage.sh
+++ manage.sh
@@ -67,7 +67,7 @@ function install_conda {
     fi
 }
 
-CONDA_ENV_TAG=2022-04-13
+CONDA_ENV_TAG=2023-01-05
 CONDA_ENV_NAME=IC-${PYTHON_VERSION}-${CONDA_ENV_TAG}
 
 function make_environment {
@@ -80,29 +80,29 @@ name: ${CONDA_ENV_NAME}
 dependencies:
 - python       = ${PYTHON_VERSION}
 # *REMEMBER TO CHANGE CONDA_ENV_TAG WHEN CHANGING VERSION NUMBERS*
-- cython       = 0.29.24
-- jupyter      = 1.0.0
-- jupyterlab   = 3.2.1
-- matplotlib   = 3.4.3
-- networkx     = 2.6.3
-- notebook     = 6.4.5
-- numpy        = 1.20.3
-- pandas       = 1.3.4
-- seaborn      = 0.11.2
-- pymysql      = 1.0.2
-- pytables     = 3.7.0
-- pytest       = 6.2.4
-- scipy        = 1.7.1
-- sphinx       = 4.2.0
-- tornado      = 6.1
-- flaky        = 3.7.0
-- hypothesis   = 6.14.1
-- pytest-xdist = 2.3.0
-- coverage     = 5.5
-- pip          = 21.2.4
-- setuptools   = 58.0.4
+- cython
+- jupyter
+- jupyterlab
+- matplotlib
+- networkx
+- notebook
+- numpy
+- pandas
+- seaborn
+- pymysql
+- pytables
+- pytest
+- scipy
+- sphinx
+- tornado
+- flaky
+- hypothesis
+- pytest-xdist
+- coverage
+- pip
+- setuptools
 - pip:
-  - pytest-instafail==0.4.2
+  - pytest-instafail
 EOF
 
     conda env create -f ${YML_FILENAME}
