01e486642fe1d8885f9773fa2a581149fd6da461
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2025-07-24 18:01:04 +0200

    Cosmetics & remove unused imports

--- invisible_cities/core/fit_functions_test.py
+++ invisible_cities/core/fit_functions_test.py
@@ -1,19 +1,16 @@
 """
 Tests for fit_functions
 """
-
 import numpy   as np
 import inspect as insp
 
 from pytest        import mark
 from pytest        import approx
 from pytest        import raises
-from flaky         import flaky
 from numpy.testing import assert_array_equal
 from numpy.testing import assert_allclose
 
 from hypothesis            import given
-from hypothesis            import settings
 from hypothesis.strategies import integers
 from hypothesis.strategies import floats
 from . testing_utils       import float_arrays
@@ -159,29 +156,23 @@ def test_covariance_matrix_values():
     assert_allclose(f.cov, expected_cov)
 
 
-@given(float_arrays(min_value=-10,
-                    max_value=10))
+@given(float_arrays(min_value=-10, max_value=10))
 def test_gauss_symmetry(data):
     assert_allclose(fitf.gauss( data, 1., 0., 1.),
                     fitf.gauss(-data, 1., 0., 1.))
 
 
-@given(integers(min_value = -100,
-                max_value = +100),
-       integers(min_value = -100,
-                max_value = +100),
-       integers(min_value = -100,
-                max_value = +100),
-       integers(min_value = -1  ,
-                max_value = +100))
+@given(integers(min_value = -100, max_value = +100),
+       integers(min_value = -100, max_value = +100),
+       integers(min_value = -100, max_value = +100),
+       integers(min_value = -  1, max_value = +100))
 def test_gauss_works_with_integers(x, a, b, c):
     args = [x, a , b, c]
     assert_allclose(fitf.gauss(*args),
                     fitf.gauss(*map(float, args)))
 
 
-@given(random_length_float_arrays(min_length = 1,
-                                  max_length = 10,))
+@given(random_length_float_arrays(min_length = 1, max_length = 10))
 def test_polynom_at_zero(pars):
     assert_allclose(fitf.polynom(0., *pars),
                     pars[0])
@@ -195,65 +186,52 @@ def test_polynom_at_one(pars):
                     np.sum(pars), rtol=1e-5)
 
 
-@given(floats(min_value = -20.,
-              max_value = -10.),
-       floats(min_value = -20.,
-              max_value = -10.))
+@given(floats(min_value = -20., max_value = -10.),
+       floats(min_value = -20., max_value = -10.))
 def test_expo_double_negative_sign(x, mean):
     assert fitf.expo(x, 1., mean) > fitf.expo(x+1., 1., mean)
 
 
-@given(floats(min_value = +10.,
-              max_value = +20.),
-       floats(min_value = +10.,
-              max_value = +20.))
+@given(floats(min_value = +10., max_value = +20.),
+       floats(min_value = +10., max_value = +20.))
 def test_expo_double_positive_sign(x, mean):
     assert fitf.expo(x, 1., mean) < fitf.expo(x+1., 1., mean)
 
 
-@given(floats(min_value = -20.,
-              max_value = -10.),
-       floats(min_value = +10.,
-              max_value = +20.))
+@given(floats(min_value = -20., max_value = -10.),
+       floats(min_value = +10., max_value = +20.))
 def test_expo_flipped_signs(x, b):
     assert fitf.expo(x, 1., b) < fitf.expo(x+1., 1., b)
 
 
-@given(integers(min_value = -10,
-                max_value = +10),
-       integers(min_value = -10,
-                max_value = +10),
-       integers(min_value = -10,
-                max_value = +10).filter(lambda x: x != 0))
+@given(integers(min_value = -10, max_value = +10),
+       integers(min_value = -10, max_value = +10),
+       integers(min_value = -10, max_value = +10).filter(lambda x: x != 0))
 def test_expo_works_with_integers(x, a, b):
     args = [x, a, b]
     assert_allclose(fitf.expo(*args),
                     fitf.expo(*map(float, args)))
 
 
-@given(floats(min_value = -10.,
-              max_value = +10.),
-       floats(min_value = -10.,
-              max_value = +10.))
+@given(floats(min_value = -10., max_value = +10.),
+       floats(min_value = -10., max_value = +10.))
 def test_power_at_one(a, b):
     assert_allclose(fitf.power(1., a, b),
                     a)
 
 
-@given(floats(min_value = -10.,
-              max_value = +10.),
-       floats(min_value =   0.,
-              max_value = +10.).filter(lambda x: x != 0.))
+@given(floats(min_value = -10., max_value = +10.),
+       floats(min_value =   0., max_value = +10.).filter(lambda x: x != 0.))
 def test_power_at_zero(a, b):
     assert_allclose(fitf.power(0., a, b),
                     0.)
 
 
-@mark.parametrize("        fn                 pars        ".split(),
-                  ((fitf.gauss         , (3.0,  2.0, 0.50)),
-                   (fitf.expo          , (6.0,  1.5)),
-                   (fitf.polynom       , (8.0, -0.5, 0.01)),
-                   (fitf.power         , (0.1,  0.8))))
+@mark.parametrize("        fn           pars        ".split(),
+                  ((fitf.gauss  , (3.0,  2.0, 0.50)),
+                   (fitf.expo   , (6.0,  1.5      )),
+                   (fitf.polynom, (8.0, -0.5, 0.01)),
+                   (fitf.power  , (0.1,  0.8      ))))
 def test_fit(fn, pars):
     pars = np.array(pars)
     x = np.arange(10.)
@@ -272,9 +250,7 @@ def test_fit_reduced_range():
     assert_allclose(f1.values, f2.values)
 
 
-@mark.parametrize("reduced".split(),
-                  ((True ,),
-                   (False,)))
+@mark.parametrize("reduced", (True, False))
 def test_fit_with_errors(reduced):
     pars = np.array([1e3, 1e2, 1e1])
     x = np.linspace(100, 300, 100)
@@ -286,9 +262,10 @@ def test_fit_with_errors(reduced):
     assert_allclose(f.values, pars)
 
 
-@mark.parametrize(["func", "known_pars"],
-                  ((fitf.gauss, {'mu' : 10, 'sigma' : 2}),
-                  (fitf.expo, {'const' : 22})))
+@mark.parametrize("     func            known_pars".split(),
+                  ( (fitf.gauss,  {'mu'    : 10, 'sigma' : 2}),
+                    (fitf.expo ,  {'const' : 22}             ),
+                   ))
 def test_number_fixed_parameters(func, known_pars):
     fixed_p        = fitf.fixed_parameters(func, **known_pars)
     npars_original = len(insp.signature(func).parameters)
@@ -332,9 +309,7 @@ def test_fixed_parameters_n_gaussians():
     assert hasattr(fixed_f, "n_gaussians")
 
 
-@mark.parametrize(["func"],
-                  ((fitf.profileX,),
-                   (fitf.profileY,)))
+@mark.parametrize("func", (fitf.profileX, fitf.profileY))
 def test_profile1D_uniform_distribution(func):
     N    = 100000
     Nbin = 100
@@ -395,11 +370,9 @@ def test_profile1D_custom_range(func, arg, ndata):
        ydata = float_arrays(size       =  100,
                             min_value  = -500,
                             max_value  =    0))
-@mark.parametrize("func  xrange".split(),
-                  ((fitf.profileX,
-                    (-100, 100)),
-                   (fitf.profileY,
-                    (-500, 0))))
+@mark.parametrize("       func         xrange   ".split(),
+                  ( (fitf.profileX, (-100, 100)),
+                    (fitf.profileY, (-500,   0))))
 def test_profile1D_full_range_x(func, xdata, ydata, xrange):
     xp, yp, ye = func(xdata, ydata)
     assert np.all(core.in_range(xp, *xrange))
@@ -414,10 +387,8 @@ def test_number_of_bins_matches(func):
     assert xp.size == yp.size == ye.size == N
 
 
-@given(xdata = float_arrays(size       =  100),
-       ydata = float_arrays(size       =  100))
 @mark.parametrize("func", (fitf.profileX, fitf.profileY))
-def test_empty_dataset_yields_nans(func, xdata, ydata):
+def test_empty_dataset_yields_nans(func):
     N = 50
     empty = np.array([])
     xp, yp, ye = func(empty, empty, N,
